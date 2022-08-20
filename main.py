98445e7bfb08a81d24cc5ffb472e2ebb62e1f4ed
Peanut S <dumbking+github@gmail.com>
2022-08-20 21:46:24 +0700

    add files

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
-from flask import Flask, redirect, url_for, request
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-
-@app.route('/success/<name>')
-def success(name):
-	return 'welcome %s' % name
-
-
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-	if request.method == 'POST':
-		user = request.form['nm']
-		return redirect(url_for('success', name=user))
-	else:
-		user = request.args.get('nm')
-		return redirect(url_for('success', name=user))
-
-
-if __name__ == '__main__':
-	app.run(debug=True,host='0.0.0.0')
+@app.route("/")
+def home():
+    return render_template("home.html")
+    
+@app.route("/about")
+def about():
+    return render_template("about.html")
+    
+if __name__ == "__main__":
+    app.run(debug=True)
