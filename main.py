47b33fa0ae15331e5421ce216b030683cc511d80
Peanut S <dumbking+github@gmail.com>
2022-08-23 10:41:40 +0700

    add contact.html

--- main.py
+++ main.py
@@ -10,5 +10,9 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
