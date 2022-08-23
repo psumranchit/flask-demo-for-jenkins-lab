d65862c6f6cd7643b2a36706e1c428e2b1d43ca4
Peanut S <dumbking+github@gmail.com>
2022-08-23 09:53:10 +0700

    add instance-id

--- scripts/start_flask.sh
+++ scripts/start_flask.sh
@@ -1,4 +1,4 @@
 MYINID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-sudo sed -i "s/REPLACE_ME/$MYINID/g" /web-demo/templates/home.html
+sudo sed -i "s/REPLACE_ME/$MYINID/g" /web-demo/templates/template.html
 
 nohup python /web-demo/main.py > /dev/null 2>&1 &
